831727908ae79d36b5a4eec6d08ad8667b8e1d91
kiranbabu189 <kiran@factweavers.com>
2019-04-01 22:19:40 +0530

    simple route to query and get results"

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
 var express = require('express');
+const Sequelize = require("sequelize");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/getQuertData', function (req, res, next) {
+
+  let dbName = "prosperoware_io_dev"
+  let instanceInfo = {
+    username: "prospadmin",
+    password: "security4cam",
+    endpoint: "devrdsmain-cluster.cluster-cvvwultysrvn.us-east-1.rds.amazonaws.com"
+  }
+  const sequelize = new Sequelize(
+    dbName,
+    instanceInfo.username,
+    instanceInfo.password,
+    {
+      host: instanceInfo.endpoint,
+      dialect: "mysql"
+    }
+  );
+
+  try {
+    let result = await queryExecutor(`select * from document limit 5`, sequelize);
+    res.send({ data: result })
+  } catch (err) {
+    console.log("error occured", JSON.stringify(err))
+    res.send({ data: err })
+  }
 });
+
+function queryExecutor(query, sequelize) {
+  return new Promise((resolve, reject) => {
+    sequelize
+      .query(query)
+      .then(users => {
+        resolve({ data: users || 0 });
+      })
+      .catch(err => {
+        reject({ data: err });
+      });
+  });
+};
 
 module.exports = router;
